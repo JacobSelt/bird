1f79dffba7b3e56fb1d3be66cd1489524b8cc5a0
JacobSelt <jak.public2@posteo.de>
2023-04-06 19:30:11 +0200

    Made project downloadable vol 2

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 Listening to birds and displaying on django webpage
 
 # TODO when deploying
-- Download birdnet and savit it to birdsongs/webapp/birdnet
 - Copy Django secret key
 - Follow digitalocean tutorial and realpython tutorial
 - 
@@ -11,3 +10,5 @@ Listening to birds and displaying on django webpage
 - https://ubuntu.com/tutorials/how-to-install-ubuntu-on-your-raspberry-pi#1-overview
 - https://realpython.com/django-nginx-gunicorn/#putting-your-site-online-with-django-gunicorn-and-nginx
 - https://www.digitalocean.com/community/tutorials/how-to-set-up-django-with-postgres-nginx-and-gunicorn-on-ubuntu-22-04#creating-the-postgresql-database-and-user
+- https://askubuntu.com/questions/1045542/how-to-stop-snapd-from-auto-updating
+- https://askubuntu.com/questions/178712/how-to-increase-swap-space
--- recordings/record.py
+++ recordings/record.py
@@ -1,9 +1,16 @@
 import sounddevice as sd
 from scipy.io.wavfile import write
 import datetime
+# import noisereduce as nr
+from decouple import config
+
+
 
 FS = 44100      # Sample rate
 SECONDS = 60    # Duration of recording
+
+# ENV
+INPUT_LOCATION = config("INPUT_LOCATION", default="input", cast=str)
 
 
 def main():
@@ -11,9 +18,11 @@ def main():
     print("#####  Starting sound recording #####")
 
     while True:
-        myrecording = sd.rec(int(SECONDS * FS), samplerate=FS, channels=2)
+        myrecording = sd.rec(int(SECONDS * FS), samplerate=FS, channels=1)
         sd.wait()
-        write(f'input/{datetime.datetime.now().strftime("%H_%M_%S_%d_%m_%Y")}.wav',
+        #myrecording = myrecording.flatten()
+        #myrecording = nr.reduce_noise(y=myrecording, sr=FS)
+        write(f'{INPUT_LOCATION}/{datetime.datetime.now().strftime("%H_%M_%S_%d_%m_%Y")}.wav',
               FS, myrecording)
 
 
